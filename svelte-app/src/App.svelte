6a5f94fbf60e69756952c5adebbc99689e5a138a
Antonin Tarrade <antotarrade@gmail.com>
2023-12-26 16:59:55 +0100

    Learning Svelte

--- svelte-app/src/App.svelte
+++ svelte-app/src/App.svelte
@@ -3,7 +3,7 @@
 </script>
 
 <main>
-	<h1>Hello {name}!</h1>
+	<h1>Hello {name.toUpperCase()}!</h1>
 	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
 </main>
 
@@ -17,7 +17,6 @@
 
 	h1 {
 		color: #ff3e00;
-		text-transform: uppercase;
 		font-size: 4em;
 		font-weight: 100;
 	}
