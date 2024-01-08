41150119d1f6625638bc9e74ebf6d8a5a42078b6
Ewen Le Bihan <hey@ewen.works>
2024-01-08 23:37:37 +0100

    use sveltekit with adapter-static

---
+++ sveltekit/src/routes/Tag.svelte
@@ -0,0 +1,18 @@
+<script>
+    export let tag = '';
+</script>
+
+<div class = 'tag'>
+    <p>{tag}</p>
+</div>
+
+<style>
+    .tag {
+        display: inline-block;
+        padding: 0em 1em;
+        margin: 0.3em;
+        border-radius: 25px;
+        background-color: #9F86C0;
+        color: #ffffff;
+    }
+</style>
