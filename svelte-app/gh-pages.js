6bd60ca9268751fa60a8b04ac889fdde337befd1
antonin-tarrade <antotarrade@gmail.com>
2024-01-08 22:57:01 +0100

    initial gh-pages commit

---
+++ svelte-app/gh-pages.js
@@ -0,0 +1,16 @@
+var ghpages = require('gh-pages');
+
+ghpages.publish(
+    'public', // path to public directory
+    {
+        branch: 'gh-pages',
+        repo: 'https://github.com/antonin-tarrade/Eportfolio', // Update to point to your repository  
+        user: {
+            name: 'antonin-tarrade', // update to use your name
+            email: 'antotarrade@gmail.com' // Update to use your email
+        }
+    },
+    () => {
+        console.log('Deploy Complete!')
+    }
+)
